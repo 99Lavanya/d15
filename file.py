93077c23406521d94a5c7fe08bce7dc9ae60e788
99Lavanya <lavanyakalamata54@gmail.com>
2026-07-21 18:32:50 +0530

    added login function

---
+++ file.py
@@ -0,0 +1,7 @@
+user_name="lavanya"
+user_password="lavvu_11"
+def login(name,password):
+    if user_name==name and user_password==password:
+        print("login successful")
+    else:
+        print("invalid credentials")    
